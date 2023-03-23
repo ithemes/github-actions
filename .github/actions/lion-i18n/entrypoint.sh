d11ccb51dd614db2bf8b503a143ea2fa14656b78
devinwalker <sdtucson@gmail.com>
2023-03-23 09:34:14 -0700

    Remove entrypoint.sh and remove_files.sh

--- .github/actions/lion-i18n/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find ../. -type f | xargs sed -i "s/\([Tt]ext [Dd]omain:\s*\)LION\b/\1it-l10n-$INPUT_TEXTDOMAIN/"
-find ../. -type f | xargs sed -i "s/\(['\"]\)LION\(['\"]\)/\1it-l10n-$INPUT_TEXTDOMAIN\2/"
